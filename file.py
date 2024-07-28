4ca12408adbe376e8fb08c80d3f02ed1d205398c
Samson Olofinsae <olofinsae.samson@gmail.com>
2024-07-28 23:01:03 +0100

    added the task questions to the upper part of file.py

---
+++ file.py
@@ -0,0 +1,8 @@
+'''
+TASK
+1. Print the XML version and encoding of the file
+2. Print the root tag
+3. Write a for loop to iterate through the child elements of the root element and print all the pairs of child tags and child attributes.
+4. Print the child elements whose attributes are empty
+
+'''
