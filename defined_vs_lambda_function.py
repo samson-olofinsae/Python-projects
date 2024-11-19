09531c3f53d06ede28eccb5f5f42c671661e6e8e
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2024-11-19 18:17:23 +0000

    Create defined_vs_lambda_function.py

---
+++ defined_vs_lambda_function.py
@@ -0,0 +1,46 @@
+# Defined function vs Lambda function
+
+# defined function
+def add(x, y):
+    result = x + y
+    return result
+
+
+print (add(4 ,10))
+
+
+# lambda function
+
+# syntax
+# lambda argument : expression
+mylamda= lambda x, y: x + y
+
+print(mylamda(6, 9))
+
+#print(mylambda(6, 7))
+
+
+# defined function
+
+def printer():
+    print('hello world')
+
+
+# lambda function
+
+mylam = lambda : 'hello world'
+
+print(mylam())
+
+
+# defined function
+def greetings(name):
+    print(f'hello {name}. This is from a defined function')
+
+
+greetings('Sam')
+
+
+# lambda function
+mylam = lambda name : f'hello {name}. This is from a lambda function'
+print(mylam('Sam'))
