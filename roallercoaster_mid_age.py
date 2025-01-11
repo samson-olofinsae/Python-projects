4121efb0b8f7cb96c8f658b2cb647a11a991758e
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-01-11 14:58:48 +0000

    Update roallercoaster_mid_age.py

--- roallercoaster_mid_age.py
+++ roallercoaster_mid_age.py
@@ -1,1 +1,28 @@
+print('wellcome to the rollercoaster!')
+height = int(input("what is your height in cm? "))
+bill = 0
 
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("what is your age? "))
+    if age < 12:
+        bill = 5
+        print("child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif 45 <= age <= 55:
+        bill = 0
+        print ("midage tickets are free") 
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride")
