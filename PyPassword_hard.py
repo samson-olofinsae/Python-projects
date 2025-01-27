98671c16370400b438d56961bdcbab52a620c676
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-01-27 18:46:37 +0000

    Update PyPassword_hard.py

--- PyPassword_hard.py
+++ PyPassword_hard.py
@@ -13,7 +13,6 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-
 nr_letters = int(input('How many letters would you like in your password?\n'))
 nr_symbols = int(input('How many symbols would you like?\n'))
 nr_numbers = int(input('How many numbers would you like?\n'))
