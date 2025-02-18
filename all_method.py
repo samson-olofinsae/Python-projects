6421416be48544351d282eea33372bcda08980ac
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-02-18 23:11:40 +0000

    Update all_method.py

--- all_method.py
+++ all_method.py
@@ -1,3 +1,12 @@
 # Using the all() method
 # iterable # tupple, dictionary, list
 # syntax all(iterable)
+
+sequence = 'ATTTGC'
+
+
+if all(ch in 'ATCG' for ch in sequence):
+    print('TRUE')
+    
+else:
+    print('FALSE')
