1e5636711614ab240b6c6f8a4ab96134895aff14
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-02-19 00:09:45 +0000

    Update test_files.py

--- test_files.py
+++ test_files.py
@@ -1,1 +1,9 @@
+# import the libraries
 
+import unittest
+
+# to test if only integar number within a list that contains both float and integars values are selected and added
+
+class test_range (unittest.TestCase):
+    def testing(self):
+        input
