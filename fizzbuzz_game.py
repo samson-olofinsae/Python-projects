a27d04d1a3c9e378c0a4d56a6a60da8de9a33e83
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-06-17 14:14:33 +0100

    Update fizzbuzz_game.py

--- fizzbuzz_game.py
+++ fizzbuzz_game.py
@@ -5,7 +5,6 @@
 # if number is divisible by 5, print Fizz
 # if number is divisible by 3 and 5, print FizzBuzz
 
-
 for number in range(1,101):
     if number % 3 == 0 and number % 5 == 0:
         number = 'FizzBuzz'
